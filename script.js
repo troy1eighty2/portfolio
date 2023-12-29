81037d9a8ad9f97531dc739648cf8cc05de3f621
troy1eighty2 <troytran000@gmail.com>
2023-12-28 19:10:29 -0600

    191012282023

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+
+        }
+        else{
+            reveals[i].classList.remove('active');
+
+        }
+
+    }
+
+}
 
 function opentab(tabname){
     for(tablink of tablinks){
